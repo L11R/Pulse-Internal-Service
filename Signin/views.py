81b88a6c6ac8f697e55bb90b14d2bdfc8f311ae0
1v1expert <1v1expert@gmail.com>
2018-04-20 17:57:53 +0300

    Add freeze pip

--- Signin/views.py
+++ Signin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def main_page(request):
+    template = loader.get_template('Signin/index.html')
+    context = {}
+    response = HttpResponse(template.render(context, request))
+    return response
