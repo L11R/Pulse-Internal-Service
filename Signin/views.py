8d29dbd952776eb28f31657521aceb5f5aa88a45
1v1expert <1v1expert@gmail.com>
2018-04-21 02:24:37 +0300

    Data checking

--- Signin/views.py
+++ Signin/views.py
@@ -3,7 +3,14 @@ from django.template import loader
 from django.http import HttpResponse
 
 def main_page(request):
+    color, msg = 'Введите ваши данные ниже', 'grey'
+    if request.method == 'POST':
+        email, password = request.POST['InputEmail'], request.POST['Inputpwd']
+        if email != 'Lerua@merlin.ru':
+            color, msg = "red", 'Неверный логин/пароль'
+        print(email, password)
     template = loader.get_template('Signin/login.html')
-    context = {}
+    color += "!important"
+    context = {'msg': msg, "color": color}
     response = HttpResponse(template.render(context, request))
     return response
