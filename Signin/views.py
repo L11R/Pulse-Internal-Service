5815802bc9309a27b4591eadcf5dc54a1d990b9e
1v1expert <1v1expert@gmail.com>
2018-04-21 03:19:29 +0300

    Add decorator that checked login users and fix views

--- Signin/views.py
+++ Signin/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 
-def main_page(request):
-    color, msg = 'Введите ваши данные ниже', 'grey'
+def login_required(func):
+    def login_required_handler(request):
+        if not request.COOKIES.get('token'):
+            return redirect('/login/')
+        return func(request)
+    return login_required_handler
+
+def login(request):
+    color, msg = "grey", "Введите ваши данные ниже"
+    #color = 'grey'
+    #response = HttpResponse()
+    #if request.COOKIES.get('token'):
+    #    return redirect('/')
     if request.method == 'POST':
         email, password = request.POST['InputEmail'], request.POST['Inputpwd']
         if email != 'Lerua@merlin.ru':
             color, msg = "red", 'Неверный логин/пароль'
-        print(email, password)
+        else:
+            response = redirect('/')
+            response.set_cookie('token', password)
+            return response
     template = loader.get_template('Signin/login.html')
     color += "!important"
     context = {'msg': msg, "color": color}
+    #response
+    response = HttpResponse(template.render(context, request))
+    #print(email, password)
+    return response
+
+@login_required
+def main_page(request):
+    template = loader.get_template('Signin/index.html')
+    context = {}
     response = HttpResponse(template.render(context, request))
     return response
