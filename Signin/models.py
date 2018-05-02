b23e7833da8914b120ca650be4b90d19a666d49f
1v1expert <1v1expert@gmail.com>
2018-05-03 00:52:18 +0300

    Make sender model

--- Signin/models.py
+++ Signin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Sender(models.Model):
+    name = models.CharField(max_length=64, verbose_name="Имя отправителя")
+    token = models.CharField(max_length=128, verbose_name="Пароль")
+    uid = models.CharField(max_length=64, verbose_name="UUID")
+    
+    def publish(self):
+        self.save()
+    def __str__(self):
+        return self.name
