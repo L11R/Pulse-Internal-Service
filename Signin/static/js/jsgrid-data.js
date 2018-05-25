1c9bb9ea3c92a16fa2a20e2446804048a6893c60
1v1expert <1v1expert@gmail.com>
2018-05-25 03:18:06 +0300

    Fix db cntrl

--- Signin/static/js/jsgrid-data.js
+++ Signin/static/js/jsgrid-data.js
@@ -21,10 +21,24 @@ $(function() {
 
         fields: [
             { name: "order_id", title: "Номер заявки", type: "text", width: 150 },
-            { name: "Age", type: "number", width: 50 },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married", sorting: false}
+            { name: "barcodes", title: "Баркод", type: "text", width: 150 },
+            { name: "terminal", title: "Терминал", type: "text", width: 150 },
+            { name: "point_settlement", title: "Город", type: "text", width: 150 },
+            { name: "point_address", title: "Адрес", type: "text", width: 150 },
+            { name: "sender", title: "Контрагент", type: "text", width: 150 },
+            { name: "consignor", title: "Отправитель", type: "text", width: 150 },
+            { name: "phone", title: "Телефон", type: "text", width: 150 },
+            { name: "date_added", title: "Дата создания", type: "text", width: 150 },
+            { name: "delivery_date", title: "Дата доставки", type: "text", width: 150 },
+            { name: "upload_date", title: "Дата получения", type: "text", width: 150 },
+            { name: "cod", title: "Наложка", type: "text", width: 150 },
+            { name: "partner_service_fee", title: "Цена партнёра", type: "text", width: 150 },
+            { name: "declared_price", title: "Объявленная ценность", type: "text", width: 150 },
+            { name: "status", title: "Статус", type: "text", width: 150 }
+            //{ name: "Age", type: "number", width: 50 },
+            //{ name: "Address", type: "text", width: 200 },
+            //{ name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
+            //{ name: "Married", type: "checkbox", title: "Is Married", sorting: false}
 
         ]
     });
