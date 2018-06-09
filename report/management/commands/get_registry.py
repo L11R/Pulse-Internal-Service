29f12ecdf7aa9ed7d6c0573d0dc11c7b52b1981d
1v1expert <1v1expert@gmail.com>
2018-06-09 20:05:25 +0300

    Fix app install

--- report/management/commands/get_registry.py
+++ report/management/commands/get_registry.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
+
 from django.core.management.base import BaseCommand
-from datetime import datetime, timedelta
-from pulse_apps.core import models
-from django.db.models import Count
-from report import models as md_report
-from decimal import Decimal
-import uuid
+
 
 
 OTYPE_MAP = {
