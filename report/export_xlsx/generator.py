139796e01af2a206cda3084a6c26142d52f62d0d
1v1expert <1v1expert@gmail.com>
2018-06-16 19:37:45 +0300

    Add Release.key for ingore files

--- report/export_xlsx/generator.py
+++ report/export_xlsx/generator.py
@@ -2,13 +2,22 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Side
 from django.conf import settings
 from collections import OrderedDict, defaultdict
+from datetime import datetime, timedelta
+from pulse_apps.core import models
+from django.db.models import Count
 
+class Defffa():
+    
+    def __init__(self):
+        self.top_row = 'Реестр'
 
-class DefaultBookkepingGenerator(object):
-    
-    def __init__(self, name):
-        self.top_row = name
-    
+    def get_events_qs(self, date):
+        return models.ParcelEvent.objects.annotate(picount=Count("parcel__items")).filter(
+        picount=0,
+        data__status__in=["Доставлена", "Выдана", "Забрана на возврат"],
+        datetime__date=date
+    )
+
     def generate(self):
         data = {
             "top_header": {
@@ -25,16 +34,31 @@ class DefaultBookkepingGenerator(object):
                 ("dt_time", "Время"),
                 ("order_id", "Номер отправки"),
                 ("barcodes", "Номер посылки"),
-                ("cell", "Номер ячейки"),
+                #("cell", "Номер ячейки"),
             ]),
             "table_data": self.do_report()
         }
         data["top_header"]["spread"] = len(data["table_header"])
         
-        return data["table_header"].values()
+        return data
         
-    def do_report(self):
-        pass
+    def do_report(self, dt=datetime(2018, 6, 10)):
+        while dt.date() <= datetime.now().date():
+            dt += timedelta(days=1)
+            for ev in self.get_events_qs(dt):
+                if int(ev.parcel.point.parcelterminal.number) in range(201, 251):
+                    yield OrderedDict([
+                        ("dpd_point_code", ev.parcel.point.parcelterminal.dpd_point_code),
+                        ("terminal", ev.parcel.point.parcelterminal.number),
+                        ("point_address", '{}, {}'.format(ev.parcel.point.settlement, ev.parcel.point.address)),
+                        ("otype", ev.data['status']),
+                        #("courier_name", "Курьер"),
+                        ("dt_date", ev.datetime.strftime('%Y.%m.%d')),
+                        ("dt_time", ev.datetime.strftime('%H:%M')),
+                        ("order_id", ev.parcel.order_id),
+                        ("barcodes", ', '.join(ev.parcel.barcodes)),
+                        #("cell", "Номер ячейки"),
+                    ])
 
 def generic(filename):
     wb = Workbook()
--- report/models.py
+++ report/models.py
@@ -47,3 +47,39 @@ PARCEL_STATUS_CHOICES = {
 #        verbose_name = 'Отчёт'
 #        verbose_name_plural = 'Отчёты'
 
+class Report(models.Model):
+    """
+    Отчётность
+    """
+    # should be parcel uid
+    idd = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4)
+    order_id = models.CharField(max_length=64, editable=True, verbose_name="Клиентский номер")
+    barcodes = models.CharField(max_length=256, verbose_name="Баркод")
+    terminal = models.CharField(max_length=8, verbose_name="Номер")
+    point_settlement = models.CharField(max_length=128, verbose_name="Населенный пункт")
+    point_address = models.TextField(verbose_name="Адрес")
+    dpd_point_code = models.CharField(max_length=32, editable=False, blank=True, default="")
+    consignor = models.CharField(max_length=128, default='', verbose_name='Отправитель')
+    date_added = models.DateTimeField(null=True, verbose_name="Дата создания")
+    status = models.SmallIntegerField(choices=PARCEL_STATUS_CHOICES, default=0, verbose_name="Статус")
+    cod = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Наложенный платёж")
+    partner_service_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0,
+                                              verbose_name="Цена услуг партнёра")
+    declared_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Объявленная ценность")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    sender = models.CharField(max_length=128, verbose_name="Контрагент")
+
+    class Meta:
+        db_table = "reports"
+        verbose_name = 'Отчёт'
+        verbose_name_plural = 'Отчёты'
+
+
+class Operation(models.Model):
+    idd = models.UUIDField(primary_key=True, editable=False, verbose_name='UID события')
+    report = models.ForeignKey(Report, verbose_name="Запись о посылке")
+    otype = models.CharField(max_length=32, verbose_name='Тип операции')
+    dt = models.DateTimeField(verbose_name='Дата и время операции UTC')
+    courier_name = models.CharField(max_length=256, null=True, verbose_name='Курьер, совершивший операцию')
+    courier_login = models.CharField(max_length=32, null=True, verbose_name='Логин курьера, совершившего операцию')
+    cell = models.CharField(max_length=3, verbose_name='Название ячейки')
