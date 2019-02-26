f9d68a9ceb29e14f97b95e6defdcee9ca9381852
1v1expert <1v1expert@gmail.com>
2019-02-26 03:10:41 +0300

    update model

--- report/models.py
+++ report/models.py
@@ -33,86 +33,84 @@ PARCEL_STATUS_CHOICES = (
 
 
 class Report(models.Model):
-    """
-    Отчётность
-    """
-    # should be parcel uid
-    idd = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4)
-    order_id = models.CharField(max_length=64, editable=True, verbose_name="Клиентский номер")
-    barcodes = models.CharField(max_length=256, verbose_name="Баркод")
-    terminal = models.CharField(max_length=8, verbose_name="Номер")
-    point_settlement = models.CharField(max_length=128, verbose_name="Населенный пункт")
-    point_address = models.TextField(verbose_name="Адрес")
-    dpd_point_code = models.CharField(max_length=32, editable=False, blank=True, default="")
-    consignor = models.CharField(max_length=128, default='', verbose_name='Отправитель')
-    date_added = models.DateTimeField(null=True, verbose_name="Дата создания")
-    status = models.SmallIntegerField(choices=PARCEL_STATUS_CHOICES, default=0, verbose_name="Статус")
-    cod = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Наложенный платёж")
-    partner_service_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0,
-                                              verbose_name="Цена услуг партнёра")
-    declared_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Объявленная ценность")
-    phone = models.CharField(max_length=20, verbose_name="Телефон")
-    sender = models.CharField(max_length=128, verbose_name="Контрагент")
-    timezone = models.CharField(max_length=32, null=True, default=None,
-                                verbose_name="Часовой пояс точки")
-    delivery_registry = models.CharField(max_length=15,  null=True, blank=True, default="", verbose_name='Номер реестра закладки')
-    backout_registry = models.CharField(max_length=156,  null=True, blank=True, default="", verbose_name='Номер реестра выемки')
-
-    class Meta:
-        db_table = "reports"
-        verbose_name = 'Отчёт'
-        verbose_name_plural = 'Отчёты'
+	"""
+	Отчётность
+	"""
+	# should be parcel uid
+	idd = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4)
+	order_id = models.CharField(max_length=64, editable=True, verbose_name="Клиентский номер")
+	barcodes = models.CharField(max_length=256, verbose_name="Баркод")
+	terminal = models.CharField(max_length=8, verbose_name="Номер")
+	point_settlement = models.CharField(max_length=128, verbose_name="Населенный пункт")
+	point_address = models.TextField(verbose_name="Адрес")
+	dpd_point_code = models.CharField(max_length=32, editable=False, blank=True, default="")
+	consignor = models.CharField(max_length=128, default='', verbose_name='Отправитель')
+	date_added = models.DateTimeField(null=True, verbose_name="Дата создания")
+	status = models.SmallIntegerField(choices=PARCEL_STATUS_CHOICES, default=0, verbose_name="Статус")
+	cod = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Наложенный платёж")
+	partner_service_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0,
+	                                          verbose_name="Цена услуг партнёра")
+	declared_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Объявленная ценность")
+	phone = models.CharField(max_length=20, verbose_name="Телефон")
+	sender = models.CharField(max_length=128, verbose_name="Контрагент")
+	timezone = models.CharField(max_length=32, null=True, default=None, verbose_name="Часовой пояс точки")
+	delivery_registry = models.CharField(max_length=15, null=True, blank=True, default="",
+	                                     verbose_name='Номер реестра закладки')
+	backout_registry = models.CharField(max_length=156, null=True, blank=True, default="",
+	                                    verbose_name='Номер реестра выемки')
+	
+	class Meta:
+		verbose_name = 'Отчёт'
+		verbose_name_plural = 'Отчёты'
 
 
 class Operation(models.Model):
-    idd = models.UUIDField(primary_key=True, editable=False, verbose_name='UID события')
-    report = models.ForeignKey(Report, verbose_name="Запись о посылке")
-    otype = models.CharField(max_length=32, verbose_name='Тип операции')
-    dt = models.DateTimeField(verbose_name='Дата и время операции UTC')
-    courier_name = models.CharField(max_length=256, null=True, verbose_name='Курьер, совершивший операцию')
-    courier_login = models.CharField(max_length=32, null=True, verbose_name='Логин курьера, совершившего операцию')
-    cell = models.CharField(max_length=3, verbose_name='Название ячейки')
+	idd = models.UUIDField(primary_key=True, editable=False, verbose_name='UID события')
+	report = models.ForeignKey(Report, verbose_name="Запись о посылке")
+	otype = models.CharField(max_length=32, verbose_name='Тип операции')
+	dt = models.DateTimeField(verbose_name='Дата и время операции UTC')
+	courier_name = models.CharField(max_length=256, null=True, verbose_name='Курьер, совершивший операцию')
+	courier_login = models.CharField(max_length=32, null=True, verbose_name='Логин курьера, совершившего операцию')
+	cell = models.CharField(max_length=3, verbose_name='Название ячейки')
 
 
-class Messages(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, verbose_name='UID сообщения')
-    date_modified = models.DateTimeField(blank=True, null=True, verbose_name="Дата модификации")
-    meta = models.CharField(max_length=550, null=True, blank=True, default="", verbose_name="Дополнительные данные")
-    order_id = models.CharField(max_length=64, null=True, blank=True, default="", verbose_name="Клиентский номер")
-    barcodes = models.CharField(max_length=150, null=True, blank=True, default="", verbose_name="Штрихкоды")
-    order_status = models.SmallIntegerField(choices=PARCEL_STATUS_CHOICES, default=0, verbose_name="Статус отправления")
-    msg_status = models.SmallIntegerField(choices=MESSAGE_STATUS_CHOICES, default=0, verbose_name="Статус сообщения")
-    
-    class Meta:
-        db_table = "Messages"
-        verbose_name = 'Сообщение'
-        verbose_name_plural = 'Сообщения'
-        ordering = ("-date_modified",)
-    
-    @property
-    def api_order_status(self):
-        return self.get_order_status_display()
-    
-    @property
-    def api_msg_status(self):
-        return self.get_msg_status_display()
-    
-    def __str__(self):
-        return 'Сообщение {} по номеру заказа №{}'.format(self.id, self.order_id)
+class Message(models.Model):
+	id = models.UUIDField(primary_key=True, editable=False, verbose_name='UID сообщения')
+	date_modified = models.DateTimeField(blank=True, null=True, verbose_name="Дата модификации")
+	meta = models.CharField(max_length=550, null=True, blank=True, verbose_name="Дополнительные данные")
+	order_id = models.CharField(max_length=64, null=True, blank=True, verbose_name="Клиентский номер")
+	barcodes = models.CharField(max_length=150, null=True, blank=True, verbose_name="Штрихкоды")
+	order_status = models.SmallIntegerField(choices=PARCEL_STATUS_CHOICES, default=0, verbose_name="Статус отправления")
+	msg_status = models.SmallIntegerField(choices=MESSAGE_STATUS_CHOICES, default=0, verbose_name="Статус сообщения")
+	
+	class Meta:
+		verbose_name = 'Сообщение'
+		verbose_name_plural = 'Сообщения'
+		ordering = ("-date_modified",)
+	
+	@property
+	def api_order_status(self):
+		return self.get_order_status_display()
+	
+	@property
+	def api_msg_status(self):
+		return self.get_msg_status_display()
+	
+	def __str__(self):
+		return 'Сообщение {} по номеру заказа №{}'.format(self.id, self.order_id)
 
 
-class Message_Statistics(models.Model):
-    id = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4)
-    date = models.DateTimeField(blank=True, null=True, verbose_name="Дата")
-    is_sent = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Отправленных")
-    is_not_sent = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Не отправленых")
-    techn_problem = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Техническая проблема")
-    new_msg = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Новых сообщений")
-    planned_msg = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Запланированных сообщений")
-    
-    class Meta:
-        db_table = "Message_Statistics"
-        verbose_name = 'Статистика'
-        verbose_name_plural = 'Статистики'
+class MessageStatistic(models.Model):
+	id = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4)
+	date = models.DateField(auto_now=True, verbose_name="Дата")
+	sent = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Отправленно")
+	unsent = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Не отправленно")
+	problem = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Проблемных")
+	new = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Новых")
+	planned = models.SmallIntegerField(null=True, blank=True, default=0, verbose_name="Запланированных")
+	
+	class Meta:
+		verbose_name = 'Статистика'
+
 
 
