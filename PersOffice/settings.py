ff36112744ec1758bb8570ecd5ecad6e3716f709
1v1expert <1v1expert@gmail.com>
2019-03-04 16:02:19 +0300

    update debug mode

--- PersOffice/settings.py
+++ PersOffice/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = DATA['SECRET_KEY']
 CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'office.pulseexpress.ru', 'office.pochtomat.ru', 'office.pochtomat.tech']
 
